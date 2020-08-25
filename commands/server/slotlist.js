e1650a91461829138e72a0229b2fe22462a3aedd
Aman9855 <amankumar.singh2019@vitstudent.ac.in>
2020-08-25 16:04:19 +0545

    Version 0.1.0

--- commands/server/slotlist.js
+++ commands/server/slotlist.js
@@ -17,7 +17,7 @@ module.exports = {
             .setTitle("Slot List")
             .setDescription(`${say}`)
             .setColor("RANDOM")
-            .addField("Id-Pass will be at  3:45 PM & 4:45  to the Leader in Specific Channel ( <#697401222478692352> ) @here <@&705783124503822337>")
+            .setDescription1("Id-Pass will be at  3:45 PM & 4:45  to the Leader in Specific Channel ( <#697401222478692352> ) @here <@&705783124503822337>")
             //.setFooter("Id-Pass will be at  3:45 PM & 4:45  to the Leader in Specific Channel ( <#697401222478692352> ) @here <@&705783124503822337>")
             .setThumbnail("https://cdn.discordapp.com/attachments/675632627998523402/722844897576747048/ezgif.com-video-to-gif_7.gif")
             
@@ -25,3 +25,4 @@ module.exports = {
             
     }
 }
+z
