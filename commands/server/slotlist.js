ba5472fefdc86994e107d946ca37f19e4c14038e
Aman9855 <amankumar.singh2019@vitstudent.ac.in>
2020-08-25 11:20:38 +0545

    Version 0.1.0

--- commands/server/slotlist.js
+++ commands/server/slotlist.js
@@ -16,7 +16,7 @@ module.exports = {
             .setAuthor(message.guild, message.guild.iconURL())
             .setTitle("Slot List")
             .setDescription(`${say}`)
-            .setField("Id-Pass will be at 3:45 PM & 4:45 to the Leader in Specific Channel")
+            //.setField("Id-Pass will be at 3:45 PM & 4:45 to the Leader in Specific Channel")
             .setColor("RANDOM")
             .setThumbnail("https://cdn.discordapp.com/attachments/675632627998523402/722844897576747048/ezgif.com-video-to-gif_7.gif")
             //.setFooter(`${message.author.username}`, `${message.author.avatarURL()}`)
